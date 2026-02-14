7af212c87e4704918d1176a8897b79c62aa6c136
drchick <jchick@bridgeport.edu>
2026-02-13 23:30:15 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
+// pages/_app.js
+import Head from "next/head";
 import "../styles/globals.css";
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Dr. Dissertation QuickLook</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* Match main site font */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
